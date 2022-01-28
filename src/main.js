ffa461b4b0105a75c80a771a32b97eebc4bfda15
Yair Marin <50755167+ipnYair95@users.noreply.github.com>
2022-01-28 14:32:06 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,8 +12,14 @@ const imprimirNumeros = () => {
   array.forEach( ele => console.log(ele) );
 }
 
+const funcionNuevaMaster() => {
+  return "Hola soy nueva funcion desde master";
+}
+
 alert( saludar('Yair') );
 
 console.log( "Resultado de sumar 2 y 5 =  ",  sumar(2, 5));
 
 imprimirNumeros();
+
+console.log( funcionNuevaMaster() );
